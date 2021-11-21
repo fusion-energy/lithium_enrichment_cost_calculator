420356e33b2332327f206198dd9e98d8d7bd4d71
Jonathan <mail@jshimwell.com>
2021-11-21 19:35:24 +0000

    added find_cost_of_enrichment fucntionality

--- lithium_enichment_cost_calculator/core.py
+++ lithium_enichment_cost_calculator/core.py
@@ -1,6 +1,6 @@
 
-
 import math
+
 
 def find_feed_amount(
     product_amount,
@@ -17,7 +17,7 @@ def find_feed_amount(
     return feed_amount
 
 
-def find_swu(
+def find_swu_amount(
     product_amount,
     product_enrichment,
     tails_amount,
@@ -25,20 +25,49 @@ def find_swu(
     feed_amount,
     feed_enrichment,
 ):
-    product_term = product_amount*(2*(product_enrichment-1.))
-    waste_term = product_amount*(2*(product_enrichment-1.))
-    product_term = product_amount*(2*(product_enrichment-1.))
+    feed_term = feed_amount*(2.*feed_enrichment-1.) * math.log(feed_enrichment/(1.-feed_enrichment))
+    tails_term = tails_amount*(2.*tails_enrichment-1.) * math.log(tails_enrichment/(1.-tails_enrichment))
+    product_term = product_amount*(2.*product_enrichment-1.) * math.log(product_enrichment/(1.-product_enrichment))
+    
+    swu = product_term+tails_term+feed_term
+
+    return swu
 
 
 def find_cost_of_enrichment(
     product_amount,
     product_enrichment,
-    feed_cost,
     swu_cost,
     tails_cost,
-    tails_enrichment
+    tails_enrichment,
+    feed_cost,
+    feed_enrichment=7.59
 ):
-    pass
+
+    feed_amount = find_feed_amount(
+        product_amount,
+        product_enrichment,
+        tails_enrichment,
+        feed_enrichment=0.079,
+    )
+
+    tails_amount = feed_amount - product_amount
+
+    swu_amount = find_swu_amount(
+        product_amount=product_amount,
+        product_enrichment=product_enrichment,
+        tails_amount=tails_amount,
+        tails_enrichment=tails_enrichment,
+        feed_amount=feed_amount,
+        feed_enrichment=feed_enrichment,
+    )
+
+    cost = swu_cost * swu_amount
+    cost =+ feed_amount * feed_cost
+    cost =- tails_amount * tails_cost
+
+    return cost
+
 
 def find_minimal_cost_of_enrichment(
     product_amount,
@@ -50,10 +79,11 @@ def find_minimal_cost_of_enrichment(
     pass
 
 if __name__ == '__main__':
-    feed_amount = find_feed_amount(
-        product_amount=100,
-        product_enrichment=60,
-        tails_enrichment=4.,
-        feed_enrichment=7.9,
-    )
-    print(feed_amount)
+    pass
+    # feed_amount = find_feed_amount(
+    #     product_amount=100,
+    #     product_enrichment=60,
+    #     tails_enrichment=4.,
+    #     feed_enrichment=7.9,
+    # )
+    # print(feed_amount)
